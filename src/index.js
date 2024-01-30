1e943e13a43b567b8bafab032f1ec69caf2d329b
Hanna-one <iamonlyone.u1@gmail.com>
2024-01-30 23:37:25 +0900

    리액트 프로젝트 생성, createBrowserRouter 세팅중

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import Main from './pages/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {path: '/', element: <Main />}
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
