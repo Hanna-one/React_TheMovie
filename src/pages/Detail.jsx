26f812e1bfe7dadbc5ae0b18afa10741a281b00b
Hanna-one <iamonlyone.u1@gmail.com>
2024-02-05 02:11:50 +0900

    Detail 페이지 슬라이드 기능 구현 완료,  영화정보 조회 기능 구현 완료

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getCredits, getImages, getMovie, getVideos } from '../api/api-function';
 import { en, genreList, gradeColors, imgPaths } from '../api/api-data';
@@ -8,18 +8,22 @@ export default function Detail() {
   const id = params.id;
   const [imageList, setImageList] = useState([]);
   const [movieInfo, setMovieInfo] = useState([]);
+  const [runtime, setRuntime] = useState([]);
   const [gradeColor, setGradeColor] = useState("");
   const [genreName, setGenreName] = useState([]);
   const [companyName, setCompanyName] = useState("");
+  const [directorList, setDirectorList] = useState([]);
+  const [producerList, setProducerList] = useState([]);
 
   // console.log(imageList.posters);
 
-    
+
   useEffect(() => {
     getMovieInformation();
+    imageSlide(imageList);
   }, [])
 
-  const setItem = (lists) => {
+  /* const setItem = (lists) => {
 
     let {
       title, vote_average, vote_count, runtime, release_date, genres,
@@ -42,65 +46,28 @@ export default function Detail() {
     setGradeColor(gradecolor)
     setGenreName(genres)
     setCompanyName(company)
-  }
+  } */
 
   const getMovieInformation = async () => {
     let imageData = await getImages(id);
-    // console.log(imageData);
-    setImageList(imageData);
     let { backdrops, posters } = imageData // let backdrops = imageData.backdrops
-    let images = backdrops.slice(0, 15)
+    let images = backdrops.slice(0, 10)
+    setImageList(images);
 
     let movieData = await getMovie(id);
     setMovieInfo(movieData);
-    // setItem(movieData);
-    // let {
-    //   title, vote_average, vote_count, runtime, release_date, genres,
-    //   overview, original_title, production_companies
-    // } = movieData
 
-    // vote_average = vote_average.toFixed(1)
-    // let gradeLevel = Math.floor(vote_average - 5)
-    // if (gradeLevel > 4) gradeLevel = 4
-    // if (gradeLevel < 0) gradeLevel = 0
-    // let gradecolor = gradeColors[gradeLevel]
-
-    // let hour = parseInt(runtime / 60) // parseInt(150/6) = 2
-    // let min = runtime % 60 // 150 % 60 = 30
-
-    // release_date = (release_date) ? release_date : '출시일 정보 없음'
-
-    // /* 
-    // [
-    //   0:{id: 28, name: '액션'}
-    //   1:{id: 53, name: '스릴러'}
-    //   2:{id: 80, name: '범죄'}
-    // ]
-    // genres.map(genre=>genre.name)
-    // ['액션','스릴러','범죄']
-    // join('/')
-    // 액션 / 스릴러 / 범죄
-    // */
-    // genres = (genres.length) ? genres.map(genre => genre.name).join('/') : '장르 정보 없음'
-    // // = genres.map((genre)=>{ return genre.name})
-
-    // if (!overview) {
-    //   let enmovieData = await getMovie(id, en)
-    //   overview = (enmovieData.overview) ? enmovieData.overview : '영화설명 정보 없음'
-    // }
-
-    // let company = (production_companies.length) ? production_companies.map(company => company.name).join(', ') : '제작사 정보 없음'
     let {
       title, vote_average, vote_count, runtime, release_date, genres,
       overview, original_title, production_companies
     } = movieData
-    vote_average = vote_average.toFixed(1)
     let gradeLevel = Math.floor(vote_average - 5)
     if (gradeLevel > 4) gradeLevel = 4
     if (gradeLevel < 0) gradeLevel = 0
     let gradecolor = gradeColors[gradeLevel]
     genres = (genres.length) ? genres.map(genre => genre.name).join('/') : '장르 정보 없음'
-    
+    release_date = (release_date) ? movieInfo.release_date : '출시일 정보 없음'
+
     if (!overview) {
       let enmovieData = getMovie(id, en)
       overview = (enmovieData.overview) ? enmovieData.overview : '영화설명 정보 없음'
@@ -111,7 +78,7 @@ export default function Detail() {
     setGradeColor(gradecolor)
     setGenreName(genres)
     setCompanyName(company)
-    
+
     let credits = await getCredits(id)
     let { cast, crew } = credits
     cast = cast.slice(0, 10)
@@ -119,9 +86,11 @@ export default function Detail() {
     crew filter. map 상세설명 복붙하기
     */
     let directors = crew.filter(v => v.job === 'Director').map(v => v.name).join(', ')
-    directors = directors ? directors : '감독 정보 없음'
+    setDirectorList(directors ? directors : '감독 정보 없음')
+    //setDirectorist(directors)
     let producers = crew.filter(v => v.job === 'Producer').map(v => v.name).join(', ')
-    producers = producers ? producers : '제작자 정보 없음'
+    setProducerList(producers ? producers : '제작자 정보 없음')
+    //setProducerList(producers)
 
     let videoData = await getVideos(id)
     let videos = videoData.results
@@ -131,18 +100,39 @@ export default function Detail() {
     }
   }
 
-  console.log(movieInfo);
+  // console.log(movieInfo);
 
+  const [slide, setSlide] = useState(false);
+  const [num, setNum] = useState(0);
+  const ref = useRef();
+
+  const imageSlide = (image) => {
+    let n = 0
+    setTimeout(() => { //화면뜨자마자 active걸면 제대로 작동이 안되므로.
+      setSlide(true)
+    }, 10)
+
+    setInterval(() => {
+      n++
+      if(n > 9) n = 1;
+      setNum(n);
+    }, 5000)
+  }
 
   return (
     <>
-      <figure className="slide">
-
+      <figure  className="slide">
+        {imageList.length < 2
+          ? imageList.map((img, idx) =>
+            <img src={`http://localhost:3000/img/film${idx}.jpg`} className={slide ? `slide-img${idx} active` : `slide-img${idx}` } alt="" key={idx}/>)
+          : imageList.map((img, idx) =>
+            <img src={`${imgPaths.original}${img.file_path}`} className={idx === num ? `slide-img${idx + 1} active` : `slide-img${idx + 1}`} alt="" key={idx}/>)
+        }
       </figure>
       <main className="detail-content">
         <section className="detail-section">
-          {imageList.posters
-            ? <img className="poster" src={`${imgPaths.w500}${imageList.posters[0].file_path}`} alt="" />
+          {movieInfo && movieInfo.poster_path
+            ? <img className="poster" src={`${imgPaths.w500}${movieInfo.poster_path}`} alt="" />
             : <img className="poster" src="http://localhost:3000/img/no-image.jpg" alt="" />
           }
           <div className="detail-info">
@@ -163,14 +153,14 @@ export default function Detail() {
                   </li>
                   <li>
                     <i className="fa-solid fa-clock-rotate-left"></i>
-                    <em className="hour">{parseInt(movieInfo.runtime / 60)}</em>
-                    <small>Hour</small>
-                    <em className="min">{movieInfo.runtime % 60}</em>
+                    <em className="hour">{movieInfo.runtime && parseInt(movieInfo.runtime / 60)}</em>
+                    <small className="hourtext">Hour</small>
+                    <em className="min">{movieInfo.runtime && movieInfo.runtime % 60}</em>
                     <small>Min</small>
                   </li>
                   <li>
                     <i className="fa-regular fa-calendar-check"></i>
-                    <small className="date">{movieInfo.release_date ? movieInfo.release_date : '출시일 정보 없음'}</small>
+                    <small className="date">{movieInfo.release_date}</small>
                   </li>
                   <li className="genres">
                     <i className="fa-solid fa-tags"></i>
@@ -182,22 +172,22 @@ export default function Detail() {
                   <li>
                     <i className="fa-solid fa-clapperboard"></i>
                     <small>제목</small>
-                    <em className="original_title"></em>
+                    <em className="original_title">{movieInfo.original_title}</em>
                   </li>
                   <li>
                     <i className="fa-solid fa-building"></i>
                     <small>제작사</small>
-                    <em className="production"></em>
+                    <em className="production">{companyName}</em>
                   </li>
                   <li>
                     <i className="fa-solid fa-user-tie"></i>
                     <small>제작자</small>
-                    <em className="producer"></em>
+                    <em className="producer">{producerList}</em>
                   </li>
                   <li>
                     <i className="fa-solid fa-user-gear"></i>
                     <small>감독</small>
-                    <em className="director"></em>
+                    <em className="director">{directorList}</em>
                   </li>
                 </ul>
               </>
